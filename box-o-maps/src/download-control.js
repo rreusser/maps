febda609141486112f71d3f259e7f0a77317086e
Ricky Reusser <572717+rreusser@users.noreply.github.com>
2025-02-11 17:24:32 -0800

    🏔️

--- box-o-maps/src/download-control.js
+++ box-o-maps/src/download-control.js
@@ -8,31 +8,117 @@ function downloadURI(uri, filename) {
   document.body.removeChild(link);
 }
 
-export default class DownloadControl {
-  constructor() { }
-
+export default class SnapshotControl {
   onAdd(map) {
-    this._controlContainer = document.createElement('div');
-    this._controlContainer.classList = `mapboxgl-ctrl mapboxgl-ctrl-group mapboxgl-ctrl-download`;
+    this._map = map;
+    this._container = document.createElement("div");
+    this._container.classList.add("mapboxgl-ctrl");
+    this._container.classList.add("mapboxgl-ctrl-group");
+    this._container.classList.add("mapboxgl-ctrl-download");
 
-    const btn = document.createElement('button');
+    const btn = document.createElement("button");
+    btn.setAttribute("aria-label", "Download snapshot");
+    btn.setAttribute("title", "Download snapshot");
+    btn.style.lineHeight = "40px";
+    btn.style.fontSize = "2em";
 
-    btn.textContent = '📷';
-    btn.title = 'Download PNG';
+    const icon = document.createElement("span");
+    icon.classList.add("mapboxgl-ctrl-icon");
+    icon.setAttribute("aria-hidden", true);
+    icon.textContent = "📷";
 
-    btn.addEventListener('click', function () {
-      map.once("render", function() {
-        downloadURI(map.getCanvas().toDataURL(), 'map.png');
-      });
-      map.triggerRepaint();
+    btn.appendChild(icon);
+
+    btn.addEventListener("click", this.download.bind(this));
+
+    this._container.appendChild(btn);
+    return this._container;
+  }
+
+  download() {
+    this._map.once("render", () => {
+      const link = document.createElement("a");
+      link.target = "_blank";
+      link.download = "map.png";
+      this.prepareImage(this._map.getCanvas())
+        .then((href) => {
+          link.href = href;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
     });
+    this._map.triggerRepaint();
+  }
 
-    this._controlContainer.appendChild(btn);
+  prepareImage(mapCanvas) {
+    const dpr = window.devicePixelRatio;
+    const canvas = document.createElement("canvas");
+    canvas.width = mapCanvas.width;
+    canvas.height = mapCanvas.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(mapCanvas, 0, 0);
 
-    return this._controlContainer;
+    ctx.scale(dpr, dpr);
+
+    const logo = this._map.getContainer().querySelector(".mapboxgl-ctrl-logo");
+    const attrib = this._map.getContainer().querySelector(".mapboxgl-ctrl-attrib");
+
+    const mapWidth = mapCanvas.width / dpr;
+    const mapHeight = mapCanvas.height / dpr;
+
+    let renderLogo = Promise.resolve();
+    if (logo) {
+      const img = new Image();
+      const imgHeight = 23;
+      const imgWidth = 88;
+      renderLogo = new Promise(function (resolve, reject) {
+        img.onload = () => resolve(img);
+        img.onerror = reject;
+      }).then((img) => {
+        if (!img) return;
+        ctx.drawImage(img, 6, mapHeight - imgHeight - 6, imgWidth, imgHeight);
+      });
+      img.src = window
+        .getComputedStyle(logo)
+        .backgroundImage.replace(/^url\(['"]/, "")
+        .replace(/['"]\)$/, "");
+    }
+
+    if (attrib) {
+      const text = attrib.textContent.replace("Improve this map", "").trim();
+      ctx.font = `${window.getComputedStyle(attrib).fontSize} ${
+        window.getComputedStyle(attrib).fontFamily
+      }`;
+      const vpad = 3;
+      const hpad = 5;
+      ctx.textAlign = "right";
+      ctx.textBaseline = "bottom";
+      const size = ctx.measureText(text);
+      const height = size.fontBoundingBoxAscent + size.fontBoundingBoxDescent;
+      ctx.fillStyle = "rgb(255 255 255 / .5)";
+      ctx.fillRect(
+        mapWidth - size.width - 2 * hpad,
+        mapHeight - height - 2 * vpad,
+        size.width + 2 * hpad,
+        height + 2 * vpad
+      );
+      ctx.fillStyle = "black";
+      ctx.fillText(
+        text,
+        mapWidth - hpad,
+        mapHeight - size.fontBoundingBoxDescent - vpad
+      );
+    }
+
+    return renderLogo.then(() => canvas.toDataURL());
   }
 
   onRemove() {
-    this._controlContainer.remove();
+    this._container.remove();
   }
 }
+
